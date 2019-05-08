3b28798755f0469ab18630b8ba1c516bf1359ca1
Sergei Pilip <psergeid@yandex.ru>
2019-05-08 22:25:23 +0300

    Add init peer

--- public/index.js
+++ public/index.js
@@ -1,3 +1,37 @@
-// Hello WebRTC
+const Peer = require("simple-peer");
 
-console.log("Page ready!");
+var peer = null;
+var stream = null;
+
+$(".call").click(function(){
+	initPeer();
+});
+
+
+
+function initPeer() {
+	peer = new Peer({
+		initiator: true,
+		trickle: false
+	});
+
+	peer.on("signal", data => {
+		out(data);
+	});
+}
+
+
+function out(text) {
+	if (typeof text == "object") text = JSON.stringify(text, true);
+	var update = $(".output").val();
+		update += text;
+		update += "\n\n";
+
+	$(".output").val(update);
+}
+
+
+
+
+
+navigator.getUserMedia({ video: true, audio: true }, s => {stream = s}, e => {out(e)});
